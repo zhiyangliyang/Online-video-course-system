3628cf7981d91218740238bb70afcfc1c6b30a6e
zhiyangliyang <1219203150@qq.com>
2022-06-13 17:12:14 +0800

    1.将system配置成最简单的Spring Boot项目

---
+++ course/system/src/main/java/com/course/system/SystemApplication.java
@@ -0,0 +1,20 @@
+package com.course.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class SystemApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(SystemApplication.class);
+        ConfigurableEnvironment environment = springApplication.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("System地址：\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
+    }
+}
